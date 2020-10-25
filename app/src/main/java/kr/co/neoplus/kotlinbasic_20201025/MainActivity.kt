87f6346c3059d9c26f7b58320eb30c35e21cb1de
kimhaun <wha02068@gmail.com>
2020-10-25 10:17:05 +0900

    메인화면 로그찍기용 버튼 배치

--- app/src/main/java/kr/co/neoplus/kotlinbasic_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/kotlinbasic_20201025/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //layout폴더에 있는 activity _main을 가져다 쓸게요.
+        //kt파일이 xml파일을 선택해서 불러온다.
     }
 }
