a8495de9476a9ca524ae7e8fe547abb5c06be55d
kimhaun <wha02068@gmail.com>
2020-10-25 10:26:53 +0900

    로그버튼을 누르면 => 로그캣에 로그 찍기

--- app/src/main/java/kr/co/neoplus/kotlinbasic_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/kotlinbasic_20201025/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.co.neoplus.kotlinbasic_20201025
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //layout폴더에 있는 activity _main을 가져다 쓸게요.
         //kt파일이 xml파일을 선택해서 불러온다.
+
+        logBtn.setOnClickListener {
+            Log.d("메인 화면", "로그 버튼 눌림")
+
+
+        }
     }
 }
